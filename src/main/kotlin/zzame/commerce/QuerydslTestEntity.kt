1e3ff7bbc6c486a0c14731fd67fd5b97d755c01b
DEVZZAME <dev.zzame@gmail.com>
2026-04-03 01:29:51 +0900

    build: verify querydsl qclass generation

---
+++ src/main/kotlin/zzame/commerce/QuerydslTestEntity.kt
@@ -0,0 +1,19 @@
+package zzame.commerce
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "querydsl_test")
+class QuerydslTestEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+
+    @Column(nullable = false, length = 100)
+    val name: String,
+)
